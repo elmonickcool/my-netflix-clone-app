97978f8a60b2aecea49ac160fca84b2594d1627f
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-04-07 16:48:53 +0800

    setup routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Navbar = () =>{
-    return(
-       <div className='flex items-center justify-between p4 z-[100] w-full absolute'>
-            <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>Netflix</h1>
-            <div>
-                <button className='text-white pr-4'>Sign in</button>
-                <button className='bg-red-6000 px-6 py-2 rounded cursor-pointer text-white'>Sign up</button>
-            </div>
-       </div>
-    )
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
+      <Link href='/'>
+        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>Netflix</h1>
+      </Link>
+      <div>
+        <Link href='/signin'>
+          <button className='text-white pr-4'>Sign in</button>
+        </Link>
+        <Link href='/signup'>
+          <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign up</button>
+        </Link>
+      </div>
+    </div>
+  )
 }
+
 export default Navbar
