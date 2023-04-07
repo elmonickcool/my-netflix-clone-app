42993803b378e0da01fdedb9caddfc083091b4c6
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-04-07 23:01:37 +0800

    add upcoming

--- src/Request.js
+++ src/Request.js
@@ -4,7 +4,8 @@ const Request = {
     requestPopularMovie :`https://api.themoviedb.org/3/movie/popular?api_key=${key}`,
     requestPopularShow:`https://api.themoviedb.org/3/tv/popular?api_key=${key}`,
     requestTrendingMovie: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
-    requestTopRatedMovie:`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}`
+    requestTopRatedMovie:`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}`,
+    requestUpcomingMovie:`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}`
 };
 
 export default Request;
