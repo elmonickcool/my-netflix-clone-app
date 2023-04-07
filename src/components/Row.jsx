bf8536f51f4985e4e57c823211857a67e7f5ca17
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-04-07 23:01:17 +0800

    revert

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -6,34 +6,30 @@ const Row = ({ title, fetchUrl }) => {
 
   useEffect(() => {
     axios.get(fetchUrl).then((response) => {
-      // shuffle the array of movies
-      const shuffledMovies = response.data.results.sort(() => Math.random() - 0.5);
-      setMovies(shuffledMovies);
+      setMovies(response.data.results);
     });
   }, [fetchUrl]);
 
   return (
     <>
       <h3 className='text-white font-bold md:text-xl p4'>{title}</h3>
-      <div className='flex flex-col md:flex-row justify-center items-center'>
-        <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide '  style={{ overflowY: 'hidden' }}>
-          {movies.map((item, id) => {
-            return (
-              <div
-                className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
-                key={id}
-              >
-                <img
-                  src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
-                  alt={item?.title}
-                />
-                <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                  <p className='white-space-normal text-base md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.name || item?.title}</p>
-                </div>
+      <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap'>
+        {movies.map((item, id) => {
+          return (
+            <div
+              className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
+              key={id}
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
+                alt={item?.title}
+              />
+              <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                <p>{item?.title}</p>
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
