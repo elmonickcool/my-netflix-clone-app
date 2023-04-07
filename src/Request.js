47c6090cb419401e9e8dc578d14ed5ed2d4bd248
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-04-07 21:40:19 +0800

    update more request

--- src/Request.js
+++ src/Request.js
@@ -3,7 +3,8 @@ const key = '86f07246c2b21c105c6052fb3a6d83fb'
 const Request = {
     requestPopularMovie :`https://api.themoviedb.org/3/movie/popular?api_key=${key}`,
     requestPopularShow:`https://api.themoviedb.org/3/tv/popular?api_key=${key}`,
-    requestTrendingMovie: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`
+    requestTrendingMovie: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
+    requestTopRatedMovie:`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}`
 };
 
 export default Request;
