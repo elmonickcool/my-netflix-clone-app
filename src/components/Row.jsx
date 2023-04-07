bb28f572c922eb62ce72b9d406091d3a385354c4
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-04-07 22:01:23 +0800

    ternary operator for media title added

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -10,14 +10,13 @@ const Row = ({ title, fetchUrl }) => {
     });
   }, [fetchUrl]);
 
-//   console.log(movies);
-
   return (
     <>
       <h3 className='text-white font-bold md:text-xl p4'>{title}</h3>
       <div className='flex flex-col md:flex-row justify-center items-center'>
         <div>
           {movies.map((item, id) => {
+            const title = item.media_type === 'tv' ? item.name : item.title;
             return (
               <div
                 className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
@@ -25,8 +24,11 @@ const Row = ({ title, fetchUrl }) => {
               >
                 <img
                   src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
-                  alt={item?.title}
+                  alt={title}
                 />
+                <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                  <p>{title}</p>
+                </div>
               </div>
             );
           })}
