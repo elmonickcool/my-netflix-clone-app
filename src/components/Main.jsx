b3fce34453e05bcae595bab3cda5b7b392600d56
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-04-07 19:51:23 +0800

    cleanup

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,28 +3,42 @@ import Request from "@/Request";
 import axios from "axios";
 
 const Main = () => {
-  const [movies, setMovies] = useState();
+    const [movies, setMovies] = useState([]);
 
-  const randomMovie =
-    movies && movies.length > 0
-      ? movies[Math.floor(Math.random() * movies.length)]
-      : null;
+    useEffect(() => {
+        axios.get(Request.requestTrendingMovie).then((response) => {
+            setMovies(response.data.results);
+        });
+    }, []);
 
-  console.log(randomMovie);
+    const randomMovie = movies.length && movies[Math.floor(Math.random() * movies.length)];
 
-  useEffect(() => {
-    axios.get(Request.requestPopularMovie).then((response) => {
-      setMovies(response.data.results);
-    });
-  }, []);
 
-  return (
-    <div className='w-full h-[550px] text-white'>
-      <div className='w-full h-full'>
-        <img className="w-full h-full" src={`https://image.tmdb.org/t/p/original${randomMovie?.backdrop_path}`} alt={randomMovie?.title} />
-      </div>
-    </div>
-  );
+    const truncateString = (str, num) => {
+        return str.length > num ? str.substr(0, num) + "..." : str;
+    }
+
+
+    return (
+        <div className='w-full h-[850px] text-white'>
+            <div className='w-full h-full'>
+                <div className="absolute w-full h-[850px] bg-gradient-to-r from-black"></div>
+                <img className="w-full h-full object-fit" src={randomMovie?.backdrop_path ? `https://image.tmdb.org/t/p/original${randomMovie?.backdrop_path}` : ""} alt={randomMovie?.media_type === "movie" ? randomMovie?.title : randomMovie?.name} />
+                <div className="absolute w-full top-[30%] p-4 md:p-8">
+                    <h1 className="text-3xl md:text-5xl font-bold mb-3">
+                        {randomMovie?.media_type === "movie" ? randomMovie?.title : randomMovie?.name}
+                    </h1>
+                    <div>
+                        <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">Play</button>
+                        <button className="border text-white border-gray-300 py-2 px-5 ml-4">More Info</button>
+                    </div>
+                    <p className="w-full md:max-w-[70%] text-gray-200 lg:max-w-[50%] xl:max-w-[35%] mt-4">
+                        {randomMovie?.overview ? truncateString(randomMovie?.overview, 255) : ""}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Main;
