3545a05a36c083b557c8cd858c223a279c639bcf
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-04-07 21:40:51 +0800

    import Row

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import Main from "@/components/Main"
+import Row from "@/components/Row"
+import Request from "@/Request"
 
 const Home = () =>{
     return(
         <>
         <Main/>
+        <Row title='Trending'  fetchUrl={Request.requestTrendingMovie}/>
+        <Row title='Top Rated' fetchUrl={Request.requestTopRatedMovie}/>
+        <Row title='Popular Movie' fetchUrl={Request.requestPopularMovie}/>
         </>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import Main from "@/components/Main"
+import Row from "@/components/Row"
+import Request from "@/Request"
 
 export default function Home() {
   return (
     <>
-    <Main/>
+      <Main />
+      <Row title='Trending' fetchUrl={Request.requestTrendingMovie} />
+      <Row title='Top Rated' fetchUrl={Request.requestTopRatedMovie} />
+      <Row title='Popular Movie' fetchUrl={Request.requestPopularMovie} />
     </>
   )
 }
