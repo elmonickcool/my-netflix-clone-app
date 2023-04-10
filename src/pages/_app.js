c73f5231c45c07c53916ffe259e4427a7197a772
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-04-11 00:13:37 +0800

    import spinner

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,44 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
+import { TailSpin } from 'react-loader-spinner'
+import React, { useState, useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+  }, [])
+  
   return (
     <>
       <Navbar />
-      <Component {...pageProps} />
+      {loading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        >
+          <TailSpin
+            height="80"
+            width="100"
+            color="#b91c1c"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      ):(
+        <Component {...pageProps} />
+      )}
+      
     </>
   )
 }
