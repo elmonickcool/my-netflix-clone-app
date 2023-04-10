0d50684c2a6d36f3c018361baebd48518d6cb3f7
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-04-11 05:58:49 +0800

    rowID

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,27 @@
 import Main from "@/components/Main"
 import Row from "@/components/Row"
 import Request from "@/Request"
+import Head from 'next/head';
+import React,{useState,useEffect} from 'react';
+
 
 const Home = () =>{
+    const [pageTitle, setPageTitle] = useState('My Default Title');
+  
+  useEffect(() => {
+    // Some code to fetch data or calculate the new title
+    const newTitle = 'Home';
+    setPageTitle(newTitle);
+  }, []);
     return(
         <>
+        <Head>
+        <title>{pageTitle}</title>
+      </Head>
         <Main/>
-        <Row title='Trending'  fetchUrl={Request.requestTrendingMovie}/>
-        <Row title='Top Rated' fetchUrl={Request.requestTopRatedMovie}/>
-        <Row title='Popular Movie' fetchUrl={Request.requestPopularMovie}/>
+        <Row title='Trending' rowID={1} fetchUrl={Request.requestTrendingMovie}/>
+        <Row title='Top Rated'rowID={2} fetchUrl={Request.requestTopRatedMovie}/>
+        <Row title='Popular Movie'rowID={3} fetchUrl={Request.requestPopularMovie}/>
         </>
     )
 }
